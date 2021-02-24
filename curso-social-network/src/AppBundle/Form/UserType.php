e88e7cacd314b999554a592109d302f8874a2316
OskrH1809 <48338394+OskrH1809@users.noreply.github.com>
2021-02-23 22:27:00 -0600

    Pagina terminada

---
+++ curso-social-network/src/AppBundle/Form/UserType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegisterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+                ->add('name',TextType::class,array(
+                    'label'=>'Nombre',
+                    'required'=>'required',
+                    'attr'=>array(
+                        'class'=>'form-name form-control',
+                        'value'=>''
+                    )
+                ))
+                ->add('surname',TextType::class,array(
+                    'label'=>'Apellido',
+                    'required'=>'required',
+                    'attr'=>array(
+                        'class'=>'form-surname form-control',
+                        'value'=>''
+                    )
+                ))
+                ->add('nick',TextType::class,array(
+                    'label'=>'Nick',
+                    'required'=>'required',
+                    'attr'=>array(
+                        'class'=>'form-nick nick-input form-control',
+                        'value'=>''
+                    )
+                ))
+                ->add('email',EmailType::class,array(
+                    'label'=>'Correo',
+                    'required'=>'required',
+                    'attr'=>array(
+                        'class'=>'form-email form-control',
+                        'value'=>''
+                    )
+                ))
+                ->add('password',PasswordType::class,array(
+                    'label'=>'Contraseña',
+                    'required'=>'required',
+                    'attr'=>array(
+                        'class'=>'form-password form-control',
+                        'value'=>''
+                    )
+                ))
+                ->add('Registrarse',  SubmitType::class, array(
+                    "attr"=>array(
+                        "class"=>"form-submit btn btn-success"
+                    )
+                ))
+
+                
+                
+                ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackendBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backendbundle_user';
+    }
+
+
+}
